7649db9bd3b92ef5084d43e100995e04c2f4eb20
marko.mobius <marko.mobius@gmail.com>
2019-01-09 11:11:45 +0100

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Appliance appliance;
+        Freezer freezer1;
+        Freezer freezer2;
+        Oven oven;
+        List<Appliance> appliances;
+
+        public Form1()
+        {
+            InitializeComponent();
+            appliances = new List<Appliance>();
+            appliance = new Oven("oven big", 200,200);
+            freezer1 = new Freezer("freezer type freezer", 122,-10);
+            freezer2 = new Freezer("freezer type freezer", 155, -20);
+            oven = new Oven("oven", 222, 500);
+            appliances.Add(appliance);
+            appliances.Add(freezer1);
+            appliances.Add(freezer2);
+            appliances.Add(oven);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "";
+            foreach (Appliance item in appliances)
+            {
+                if(item is IFreeze)
+                {
+                    message += item.WhoAmI();
+                    IFreeze newRef;
+                    newRef = item as IFreeze;
+                    message+=newRef.Freeze()+"\r\n";
+                }
+                else
+                {
+                    message+= "I am a "+item.Description+" and my power consumption is: "+item.PowerConsumption+"\r\n";
+                }
+            }
+            label1.Text = message;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Interface examples.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+
+    interface IFreeze
+    {
+        string Freeze();
+        
+        int FreezingTemperature { get; set; }
+    }
+
+    abstract class Appliance
+    {
+        public Appliance(string description, int powerConsumption)
+        {
+            Description = description;
+            PowerConsumption = powerConsumption;
+        }
+        internal virtual string WhoAmI()
+        {
+            return "I am a base class appliance\r\n";
+        }
+
+        public string Description { get; set; }
+        public int PowerConsumption { get; set; }
+        
+    }
+    class Oven : Appliance
+    {
+        public Oven(string description, int powerConsumption, int capacity) : base(description, powerConsumption)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+        internal override string WhoAmI()
+        {
+            return "I am an oven and I am derived from: \r\n"+base.WhoAmI();
+        }
+
+    }
+    class Freezer : Appliance, IFreeze
+    {
+        public Freezer(string description, int powerConsumption, int freezingTemp) : base(description, powerConsumption)
+        {
+            FreezingTemperature = freezingTemp;
+        }
+
+        public int FreezingTemperature { get ; set ; }
+
+        internal override string WhoAmI()
+        {
+            return "I am freezer and I am derived from: \r\n" + base.WhoAmI();
+        }
+        public string Freeze()
+        {
+            return "I am a freezer and my freezing temp is: "+FreezingTemperature;
+        }
+    }
+}
